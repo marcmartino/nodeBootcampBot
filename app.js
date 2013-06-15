9db48dd05389dad822b192927037c5ae2154d3c8
Marc Martino <mrmarcmartino@gmail.com>
2013-06-15 13:26:30 -0700

    added express server

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var irc = require('irc'),
+	express = require('express'),
 	botFunctions = require('./botFunctions'),
 	config = {
 		channels: ["#nodejsbootcamp"],
@@ -12,6 +13,12 @@ var irc = require('irc'),
 	bot = new irc.Client(config.server, config.botName, {
     channels: config.channels
   });
+
+  var app = express();
+	app.set('port', process.env.PORT || 3000);
+	app.get("/", function (req, res) {
+		res.send("hello world");
+	})
 
   botFunctions = botFunctions(bot);
   botFunctions.actions = {
@@ -29,11 +36,15 @@ var irc = require('irc'),
 	// });
 
 	bot.addListener("message", function(from, to, text, message) {
-		
-		if ((text.indexOf(config.messageAlias) + text.indexOf(config.botName)) >= -1) {
+		var messageAliasSpot = text.indexOf(config.messageAlias),
+			botNameSpot = text.indexOf(config.botName);
+		if (messageAliasSpot >= 0) {
 			console.log("it's time to go");
 			botFunctions.tellUser(config.channels[0], from, "i agree");
-
+		else if (botNameSpot >= 0)) {
+			console.log("it's time to go");
+			var cleanedMessage = botFunctions.cleanIrcMessage(config.botName)
+			botFunctions.processMessage()
 		} else {
 			console.log("it's not time to go!");
 		}
