e07fb495ec9b316c61dde9aeaed0e0d39e5c7ce0
Marc Martino <mrmarcmartino@gmail.com>
2013-06-15 12:31:22 -0700

    initial functionality

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-console.log("poops");
+var irc = require('irc'),
+	config = {
+		channels: ["#nodejsbootcamp", "nodebootcamp"],
+		server: "irc.freenode.net",
+		botName: "marcBot"
+	},
+	bot = new irc.Client(config.server, config.botName, {
+    channels: config.channels
+  });
+
+	bot.addListener("join", function(channel, who) {
+		// Welcome them in!
+		bot.say(channel, who + "...dude...welcome back!");
+	});
