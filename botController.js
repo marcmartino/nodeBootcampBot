2c8fb87ea49925090844499aeb7881e99310ef41
Marc Martino <mrmarcmartino@gmail.com>
2013-06-15 15:14:49 -0700

    got texting integrated

--- botController.js
+++ botController.js
@@ -3,13 +3,24 @@ var botFunctions = require('./botFunctions');
 module.exports = function (bot, config) {
 	botFunctions = botFunctions(bot);
 	botFunctions.actions = {
-		"eval-": botFunctions.javascriptEval
+		"eval-": botFunctions.javascriptEval,
+		"HoT?": botFunctions.headsOrTails,
+	};
+	botFunctions.addressBook = {
+		"marcM": "7022902481",
+		"marcAM": "7022902481"
 	}
 
 	console.log("starting up"); 
 
 	bot.addListener('error', function(message) {
 	    console.log('error: ', message);
+	});
+
+	bot.addListener("names#" + config.channels[0], function (nicks) {
+		console.log("nicknames incoming");
+
+		botFunctions.nicks = nicks;
 	});
 
 	// bot.addListener("join", function(channel, who) {
@@ -27,6 +38,7 @@ module.exports = function (bot, config) {
 			var cleanedMessage = botFunctions.cleanIrcMessage(text, config.botName);
 			botFunctions.processMessage(to, from, cleanedMessage);
 		} else {
+			botFunctions.processNonMessage(to, from, text);
 			console.log("it's not time to go!");
 		}
 	});
--- botFunctions.js
+++ botFunctions.js
@@ -2,6 +2,16 @@ module.exports = function (bot) {
 	var _this;
 	return {
 		actions: {},
+		addressBook: {
+			"marcM": "7022902481",
+			"marcAM": "7022902481"
+		},
+		nicks: [
+			"marcBot",
+			"azureBot",
+			"thisGuy",
+			'AndChat39802'
+		],
 		javascriptEval: function (channel, user, command) {
 			console.log('javascript eval');
 			_this.tellUser(channel, user, eval(command));
@@ -47,8 +57,35 @@ module.exports = function (bot) {
 			//console.log(actionKeys[actionFound]);
 			actionString = actionKeys[actionFound];
 			actionFunction = this.actions[actionString];
-			actionParameter = (command | "").substring(actionString.length);
+			actionParameter = (command).substring(actionString.length);
 			actionFunction(channel, user, actionParameter);
+		},
+		processNonMessage: function (channel, user, message) {
+			var cleanedMessage,
+				userFound = false,
+				thisRegex = /[^ ]+,/;
+			if (!_this) {_this = this; }
+			for (var m = _this.nicks.length - 1; m >= 0; m--) {
+				if (message.indexOf(_this.nicks[m]) === 0) {
+					//cleanedMessage = newTrim(message.substr(this.nicks[m].length));
+					userFound = true;
+				}
+			}
+			if (!userFound && (message + ", ").search(thisRegex) === 0) {
+				var foundText = message.substr(message.indexOf(", ") + 2);
+				console.log("found possible rogue candidate");
+				var exec = require('child_process').exec;
+				exec('curl -X POST https://api.twilio.com/2010-04-01/Accounts/ACf4998dded0758c6c977d5f000bf4e0c2/SMS/Messages.json \
+			    -u ACf4998dded0758c6c977d5f000bf4e0c2:67a4e6ba8f77d94691879664014da658 \
+			    -d "From=+17027897643" \
+			    -d "To=+17022902481" \
+			    -d "Body=' + foundText + '\nFrom: ' + user + '"');
+				/*curl -X POST https://api.twilio.com/2010-04-01/Accounts/ACf4998dded0758c6c977d5f000bf4e0c2/SMS/Messages.json \
+			    -u ACf4998dded0758c6c977d5f000bf4e0c2:67a4e6ba8f77d94691879664014da658 \
+			    -d "From=+17027897643" \
+			    -d "To=+17022902481" \
+			    -d 'Body=hello'*/
+			}
 		}
 	};
 };
